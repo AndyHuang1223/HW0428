8902b49e3bd27da225fde18c8f1df229a50177b5
AndyHuang <andyhuang1223@gmail.com>
2026-04-28 14:48:39 +0800

    feat: 作業二(Array.Sort 版本)

--- NumJudge2/Program.cs
+++ NumJudge2/Program.cs
@@ -8,30 +8,61 @@
             var input = Console.ReadLine();
             //切,
             var arr = input.Split(',');
+
+            var tempNumArr = new int[arr.Length];
+
+            for(var i = 0; i < tempNumArr.Length; i++)
+            {
+                tempNumArr[i] = int.Parse(arr[i]);
+            }
+
+            //先排序
+
+            Array.Sort(tempNumArr);//排原本陣列(就地排)
+            
+            
+            // 5:  0,0,0,0,0
             //奇數的List 偶數的List
             var oddList = new List<int>();
             var evenList = new List<int>();
 
-            foreach(var item in arr)
+            foreach(var item in tempNumArr)
             {
-                var num = int.Parse(item);
-
-                if (IsOdd(num))
+                if (IsOdd(item))
                 {
-                    oddList.Add(num);
+                    oddList.Add(item);
                 }
                 else
                 {
-                    evenList.Add(num);
+                    evenList.Add(item);
                 }
             }
 
-            //排序 3,5,1,7
-            var sortedOddResult = oddList.OrderBy((num) => num);
-            var sortedEvenResult = evenList.OrderBy(num => num);
+            Console.WriteLine($"奇數: {string.Join(",", oddList)}");
+            Console.WriteLine($"偶數: {string.Join(",", evenList)}");
 
-            Console.WriteLine($"奇數: {string.Join(",", sortedOddResult)}");
-            Console.WriteLine($"偶數: {string.Join(",", sortedEvenResult)}");
+
+
+            //foreach(var item in arr)
+            //{
+            //    var num = int.Parse(item);
+
+            //    if (IsOdd(num))
+            //    {
+            //        oddList.Add(num);
+            //    }
+            //    else
+            //    {
+            //        evenList.Add(num);
+            //    }
+            //}
+
+            ////排序(回傳新的) 3,5,1,7
+            //var sortedOddResult = oddList.OrderBy((num) => num);
+            //var sortedEvenResult = evenList.OrderBy(num => num);
+
+            //Console.WriteLine($"奇數: {string.Join(",", sortedOddResult)}");
+            //Console.WriteLine($"偶數: {string.Join(",", sortedEvenResult)}");
 
         }
 
