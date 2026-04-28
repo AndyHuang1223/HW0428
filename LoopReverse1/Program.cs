bd19c63fc3dd3b712d684d5ce7f3d7178f3e3710
AndyHuang <andyhuang1223@gmail.com>
2026-04-28 15:53:37 +0800

    feat: 第三題

---
+++ LoopReverse1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LoopReverse1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入用,分隔的字串");
+            var input = Console.ReadLine();
+            var strArr = input.Split(',');
+
+            for(var i = 0; i < strArr.Length / 2; i++)
+            {
+                var targetIdx = strArr.Length - 1 - i;
+                var temp = strArr[targetIdx];
+                strArr[targetIdx] = strArr[i];
+                strArr[i] = temp;
+            }
+
+            Console.WriteLine(string.Join(",", strArr));
+        }
+    }
+}
