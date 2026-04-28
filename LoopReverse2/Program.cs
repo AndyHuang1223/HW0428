c2116a7d7707169f10395cfffc32b16740e0e765
AndyHuang <andyhuang1223@gmail.com>
2026-04-28 16:07:44 +0800

    feat: 作業四

---
+++ LoopReverse2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LoopReverse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入1-9的數字: ");
+            var input = Console.ReadLine();
+            var inputNum = int.Parse(input); //3
+            var layer = 1;
+
+            for(int num = inputNum; num > 0; num--)
+            {
+                for(int j = 0; j < layer; j++)
+                {
+                    Console.Write(num);
+                }
+                layer++;
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
