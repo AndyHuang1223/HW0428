c8556da224bf33f19a4339bd765373ad9fcbadfd
AndyHuang <andyhuang1223@gmail.com>
2026-04-28 16:31:01 +0800

    feat: 作業五

---
+++ JudgeLeapYear/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace JudgeLeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入民國年:");
+            var input = Console.ReadLine();
+            var mingoYear = int.Parse(input);
+
+            var twCal = new TaiwanCalendar();
+
+            if (twCal.IsLeapYear(mingoYear))
+            {
+                Console.WriteLine("是閏年");
+            }
+            else
+            {
+                Console.WriteLine("不是閏年");
+            }
+
+
+            var year = mingoYear + 1911;
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("是閏年");
+            }
+            else
+            {
+                Console.WriteLine("不是閏年");
+            }
+        }
+    }
+}
